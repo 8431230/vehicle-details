afa253805175eb2f1d00a4252326b52076cc4210
Soumya <bangaragundsoumya@gmail.com>
2025-12-27 12:24:17 +0530

    save

---
+++ vehicle.py
@@ -0,0 +1,13 @@
+def vehicle_details(v_ID,name,price,yop):
+    result=(
+        f"Vehicle ID: {v_ID}\n"
+        f"Name: {name}\n"
+        f"Price: ${price}\n"
+        f"Year of Purchase: {yop}\n" 
+              )
+    return result
+if __name__ == "__main__":
+    v_ID="V1234"
+    name="car"
+    price=24000
+    yop=2021
